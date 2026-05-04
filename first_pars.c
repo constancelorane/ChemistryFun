1dd10cca0aa13f4c387fe0a5e1e86794b0e0a8ca
Gorbushin Danil <danekgorbushin@mail.ru>
2026-05-04 22:27:55 +0700

    Добавил поиск реактива по базе данных

--- first_pars.c
+++ first_pars.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cjson/cJSON.h>
-char* first_pars(const char* filename){ // Функция для первичного парсинга файла
-    FILE* f = fopen(filename, "r"); // Задаем имя файла f открытием data только для чтения
+char *first_pars(const char *filename){ // Функция для первичного парсинга файла
+    FILE *f = fopen(filename, "r"); // Задаем имя файла f открытием data только для чтения
     if (!f) return NULL; // Если файл по какой-либо причине не открылся, возвращаем null
     fseek(f, 0, SEEK_END); // Перемещаем курсор в конец файла
     long size = ftell(f); // размер файла = позиция курсора
     rewind(f); // Возвращаем курсор в начало файла
-    char* data = malloc(size + 1); // Выделение необходимого количества памяти с учетом '\0'
+    char *data = malloc(size + 1); // Выделение необходимого количества памяти с учетом '\0'
     if (!data) { // Если памяти не хватит, то вернется null
         fclose(f);
         return NULL;
--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <cjson/cJSON.h>
 
 #include "rastvorenie.h"
 #include "razbavlenie.h"
+#include "first_pars.h"
 int main() {
+  char *data = first_pars("data.json"); // Выгрузка базы данных в память 
+  if(!data){
+    printf("Ошибка чтения файла");
+    return 1;
+  }
+  printf("Введите название реактива.\n");
+  char name[50]; // Место хранение вводимого реактива
+  if(fgets(name, sizeof(name), stdin)){ // Прием вводимого значение и запись его в name
+    name[strcspn(name, "\n")] = '\0'; // Замена \n в конце на \0
+  }
+  cJSON *root = cJSON_Parse(data); // Построение древа для чтения базы данных
+  if(!root){
+    printf("Ошибка парсинга.");
+    free(data);
+    return 1;
+  }
+  cJSON *comp = cJSON_GetObjectItem(root, name);  // Поиск нужного реактива по древу
+  if(!comp){
+    printf("Реактива нет в базе данных.\n");
+    printf("Необхожимо ввести некоторые данные вручную.\n");
+  }
+  else printf("Реактив найден.\n");
+  
+
+  
+  
+
+
   printf("Выберите желаемый расчет.\n");
   printf("1 - Растворение твердого вещества в воде в мерной колбе.\n");
   printf("2 - Разбавлене жидкого реагента.\n");
